ebf8da8056d2d5267befdcf408452933f1944ac8
anonymous-YM <31094168+anonymous-YM@users.noreply.github.com>
2018-11-20 20:13:29 -0700

    Add files via upload

---
+++ apps/secondarysort/dataset/random.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+int n = 100000;
+string last[50], first;
+for (int j = 0; j < 50; j++)
+	for (int i = rand() % 10 + 1; i > 0; i--) {
+			last[j] += char(rand() % 26 + 'a');
+		}
+
+while (n--) {
+
+	cout << last[rand() % 50];
+	cout << ',';
+	first = "";
+	for (int i = rand() % 10 + 1; i > 0; i--) {
+		first += char(rand() % 26 + 'a');
+	}
+	cout << first << endl;
+
+}
+}
